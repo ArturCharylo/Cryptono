dcab81d5069332209deb9ae5877c00c335a3b270
Artur <artur.cha@outlook.com>
2025-12-08 16:35:52 +0100

    Added validation for just password in regex and create a util for generating strong passwords

---
+++ cryptono/src/utils/passGen.ts
@@ -0,0 +1,27 @@
+import { passwordRegex } from "../validation/validate";
+
+export const generateStrongPassword = (length: number = 16): string => {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    const array = new Uint32Array(length);
+    
+    // Loop for handling the unlikely event of wrong password format
+    let attempts = 0;
+    while (attempts < 50) { // Safety break, avoids overload
+        window.crypto.getRandomValues(array);
+        let password = '';
+        
+        for (let i = 0; i < length; i++) {
+            password += charset[array[i] % charset.length];
+        }
+
+        // Regex validation
+        if (passwordRegex.test(password)) {
+            return password;
+        }
+        
+        attempts++;
+    }
+
+    // Fallback if in 50 attempts the random function doesn't generate any correct passwords wich is way less than unlikely
+    return "ErrorGeneratingStrongPass1!"; 
+};
