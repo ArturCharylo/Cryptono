7e4265cfb00d0c0649e07c52ccb69da96a0c6a28
Artur <artur.cha@outlook.com>
2025-12-11 10:33:15 +0100

    Added Toast and custom error messags to AddItems file

--- cryptono/src/components/AddItem.ts
+++ cryptono/src/components/AddItem.ts
@@ -3,6 +3,7 @@ import { STORAGE_KEYS } from "../constants/constants";
 import type { VaultItem } from "../types";
 import { addValidation } from "../validation/validate";
 import { generateStrongPassword } from "../utils/passGen";
+import { clearField, setErrorMessage, setInputClassError, showToastMessage, ToastType } from '../utils/messages';
 
 export class AddItem {
     navigate: (path: string) => void;
@@ -25,11 +26,13 @@ export class AddItem {
                 <form class="login-form" id="item-form">
                     <div class="input-group">
                         <label for="url">URL</label>
-                        <input type="text" name="url" id="url" placeholder="example.com" class="form-input" required/>
+                        <input type="text" name="url" id="url" placeholder="example.com" class="form-input"/>
+                        <div class="input-error" id="url-error"></div>
                     </div>
                     <div class="input-group">
                         <label for="username">Username</label>
-                        <input type="text" name="username" id="username" placeholder="Enter your username" class="form-input" required/>
+                        <input type="text" name="username" id="username" placeholder="Enter your username" class="form-input"/>
+                        <div class="input-error" id="username-error"></div>
                     </div>
                     
                     <div class="input-group">
@@ -45,8 +48,8 @@ export class AddItem {
                                 id="password" 
                                 placeholder="Str0ng_P@Ssword" 
                                 class="form-input form-input-with-icon" 
-                                required
                             />
+                            <div class="input-error" id="password-error"></div>
                             <button 
                                 type="button" 
                                 id="toggle-pass-visibility" 
@@ -58,7 +61,8 @@ export class AddItem {
 
                     <div class="input-group">
                         <label for="re-pass">Repeat Password</label>
-                        <input type="password" name="re-pass" id="re-pass" placeholder="Repeat your password" class="form-input" required/>
+                        <input type="password" name="re-pass" id="re-pass" placeholder="Repeat your password" class="form-input"/>
+                        <div class="input-error" id="re-pass-error"></div>
                     </div>
 
                     <button type="submit" class="login-btn save-btn">
@@ -84,6 +88,19 @@ export class AddItem {
         const form = document.getElementById('item-form') as HTMLFormElement;
         const cancelBtn = document.getElementById('cancel-btn');
         const submitBtn = form.querySelector('button[type="submit"]') as HTMLButtonElement;
+        // Handle displaying errors for better UX
+        const inputList = form.querySelectorAll('.form-input') as NodeListOf<HTMLInputElement>
+
+        
+        for (const input of inputList) {
+            input.addEventListener('input', () => {
+                setInputClassError(input, false);
+                const errorDiv = document.getElementById(`${input.id}-error`);
+                if (errorDiv) {
+                    clearField(errorDiv);
+                }
+            });
+        }
 
         // Password generator elements
         const genBtn = document.getElementById('gen-pass-btn');
@@ -130,13 +147,36 @@ export class AddItem {
         form?.addEventListener('submit', async (e) => {
             e.preventDefault();
 
-            const url = (document.getElementById('url') as HTMLInputElement).value;
-            const username = (document.getElementById('username') as HTMLInputElement).value;
-            const password = (document.getElementById('password') as HTMLInputElement).value;
-            const rePass = (document.getElementById('re-pass') as HTMLInputElement).value;
+            // Error handling
+            let isValid: boolean = true;
+            const formData = new FormData(form);
+
+            // Form fields
+            const url = formData.get('url') as string;
+            const username = formData.get('username') as string;
+            const password = formData.get('password') as string;
+            const rePass = formData.get('re-pass') as string;
+
+            if (!url || !username || !password || !rePass){
+            for (const input of inputList) {
+                    const errorDiv = document.getElementById(`${input.id}-error`);
+                    if (errorDiv) {
+                        clearField(errorDiv);
+                        if (!input.value) {
+                            setInputClassError(input, true);
+                            setErrorMessage(errorDiv, 'This field is required');
+                        } 
+                    }
+                }
+                isValid = false;
+            }
+
+            if (!isValid) {
+                return;
+            }
 
             if (password !== rePass) {
-                alert("Passwords do not match!");
+                showToastMessage('Passwords do not match', ToastType.ERROR, 2500)
                 return;
             }
 
@@ -170,16 +210,16 @@ export class AddItem {
 
                 this.navigate('/passwords');
 
-            } catch (error) {
-                console.error(error);
-                
+            } catch (error) {            
                 // handle expired sessions
                 if ((error as Error).message.includes("Session expired")) {
                     alert((error as Error).message);
                     this.navigate('/login');
+                }
+                else if (error instanceof Error) {
+                    showToastMessage(error.message, ToastType.ERROR, error.message.length > 50 ? 6000 : 2500);
                 } else {
-                    // Show valiadtion or saving errors
-                    alert((error as Error).message);
+                    showToastMessage('An unexpected error occurred.', ToastType.ERROR, 2500);
                 }
             } finally {
                 // Reset loading state
