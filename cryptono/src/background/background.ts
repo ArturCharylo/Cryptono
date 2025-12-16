a0542c43dbeea832dc0f3afb92f439728fcd620f
Artur <50996474+ArturCharylo@users.noreply.github.com>
2025-12-16 23:11:17 +0100

    Connected logic, AutoSave should work now

--- cryptono/src/background/background.ts
+++ cryptono/src/background/background.ts
@@ -1,4 +1,6 @@
 import { handleAutofill } from '../handlers/AutoFillHandler';
+import { handleInputSave } from '../handlers/AutoSaveHandler';
+import { showToastMessage, ToastType } from '../utils/messages';
 
 // Wait for call from ContentScript
 chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
@@ -11,7 +13,8 @@ chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
 
   // Placeholder for future Auto Save logic
   if (message.type === 'AUTOSAVE_REQUEST') {
-    // example: handleAutosave(message.data, sendResponse);
+    handleInputSave(message.data).then(sendResponse);
+    showToastMessage("Login data saved", ToastType.SUCCESS, 2500) // Notify user that the data was saved correctly
     return true;
   }
 
