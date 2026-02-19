ac9595435c91ca459871806d9c19b8e1e976b373
Artur <50996474+ArturCharylo@users.noreply.github.com>
2026-02-19 12:34:01 +0100

    Added wasm compressor files, and updated vite config to copy them into dist

---
+++ cryptono/src/wasm/cryptono_zip_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const compress_brotli: (a: number, b: number, c: number) => [number, number];
+export const compress_deflate: (a: number, b: number) => [number, number];
+export const decompress_brotli: (a: number, b: number) => [number, number];
+export const decompress_deflate: (a: number, b: number) => [number, number];
+export const main_js: () => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_start: () => void;
