ff7ec2a96db1c56fa6463563c8ccbaa40dce16b0
Artur <50996474+ArturCharylo@users.noreply.github.com>
2026-03-09 13:16:24 +0100

    sketch of how the audit will be preformed

--- cryptono/src/services/AuditService.ts
+++ cryptono/src/services/AuditService.ts
@@ -1,7 +1,21 @@
 import init, { perform_local_audit } from '../wasm/cryptono_audit';
+import { VaultRepository } from '../repositories/VaultRepository';
 
-export async function runAudit(data: Uint8Array) {
+async function runAudit(data: Uint8Array) {
     await init();
     const result = perform_local_audit(JSON.stringify(data));
     return JSON.parse(result);
+}
+
+export class AuditService {
+    private vaultRepository: VaultRepository;
+
+    constructor(vaultRepository: VaultRepository) {
+        this.vaultRepository = vaultRepository;
+    }
+
+    async results() {
+        const vault = this.vaultRepository.getAllItems();
+        return runAudit(vault as unknown as Uint8Array);
+    }
 }
