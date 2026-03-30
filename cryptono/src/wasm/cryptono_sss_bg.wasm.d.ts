f9d1be805d1866998f8f90414ecd5149efcbf937
Artur <50996474+ArturCharylo@users.noreply.github.com>
2026-03-30 12:39:47 +0200

    files: added wasm for SSS and updated vite config

---
+++ cryptono/src/wasm/cryptono_sss_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_backupshares_free: (a: number, b: number) => void;
+export const backupshares_get_share: (a: number, b: number) => any;
+export const backupshares_len: (a: number) => number;
+export const generate_backup_codes: (a: number, b: number, c: any, d: number, e: number) => [number, number, number];
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
